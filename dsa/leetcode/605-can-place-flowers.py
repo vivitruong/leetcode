ab0abf8831f02094bfb8dfe327ed0cd100cb4fd6
vivitruong <vivitruong1506@gmail.com>
2024-02-20 22:31:55 -0600

    605

---
+++ dsa/leetcode/605-can-place-flowers.py
@@ -0,0 +1,24 @@
+# You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+# Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+
+
+# Example 1:
+
+# Input: flowerbed = [1,0,0,0,1], n = 1
+# Output: true
+# Example 2:
+
+# Input: flowerbed = [1,0,0,0,1], n = 2
+# Output: false
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        f = [0] + flowerbed + [0]
+
+        for i in range(1, len(f) - 1): #skip first and last
+            if f[i-1] == 0 and f[i] == 0 and f[i+1] == 0:
+                f[i] = 1
+                n -= 1
+        return n<= 0
