92535c5ba75b6589a6b36b07efba08c301576c28
vivitruong <vivitruong1506@gmail.com>
2024-03-04 21:53:30 -0600

    add 1189

---
+++ dsa/leetcode/1189-max-num-of-baloons.py
@@ -0,0 +1,9 @@
+# Given a string text, you want to use the characters of text to form as many instances of the word "balloon" as possible.
+
+# You can use each character in text at most once. Return the maximum number of instances that can be formed.
+# Input: text = "loonbalxballpoon"
+# Output: 2
+# Example 3:
+
+# Input: text = "leetcode"
+# Output: 0
