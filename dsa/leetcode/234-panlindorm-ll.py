89c5c7db19fdd6fff505580919f53cf3eab02b50
vivitruong <vivitruong1506@gmail.com>
2024-05-20 21:18:27 -0500

    add 234

---
+++ dsa/leetcode/234-panlindorm-ll.py
@@ -0,0 +1,51 @@
+# iven the head of a singly linked list, return true if it is a
+# palindrome
+#  or false otherwise.
+
+
+
+# Example 1:
+
+
+# Input: head = [1,2,2,1]
+# Output: true
+# Example 2:
+
+
+# Input: head = [1,2]
+# Output: false
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        fast = head
+        slow = head
+
+        # find the middle (slow)
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        # reverse second half
+        prev = None
+        while slow:
+            tmp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = tmp
+
+        # check palindrome
+        left, right = head, prev
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        return True
--- dsa/leetcode/88-merge-sorted-array.py
+++ dsa/leetcode/88-merge-sorted-array.py
@@ -22,7 +22,7 @@ class Solution:
 
 
 
-#             You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+#    You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
 
 # Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 
