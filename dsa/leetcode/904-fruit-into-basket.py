0cb61c2931578ee7ee9ccd9b34021bb04837fb5b
vivitruong <vivitruong1506@gmail.com>
2024-05-27 22:27:10 -0500

    add 904 and finish

--- dsa/leetcode/904-fruit-into-basket.py
+++ dsa/leetcode/904-fruit-into-basket.py
@@ -26,3 +26,27 @@
 # Output: 4
 # Explanation: We can pick from trees [2,3,2,2].
 # If we had started at the first tree, we would only pick from trees [1,2].
+class Solution(object):
+    def totalFruit(self, fruits):
+        """
+        :type fruits: List[int]
+        :rtype: int
+        """
+        count = collections.defaultdict(int)
+        l, total, res = 0, 0, 0
+
+        for r in range(len(fruits)):
+            count[fruits[r]] += 1
+            total += 1
+
+            while len(count) > 2:
+                f = fruits[l]
+                count[f] -= 1
+                total -= 1
+                l += 1
+                if not count[f]:
+                    count.pop(f)
+
+            res = max(res, total)
+
+        return res
