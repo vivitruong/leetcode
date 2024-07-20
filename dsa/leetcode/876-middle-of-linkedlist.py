26a86e4e1fbda542ea50c67d821a5df4ac5839cf
vivitruong <vivitruong1506@gmail.com>
2024-07-19 21:56:20 -0500

    add 876 fix

--- dsa/leetcode/876-middle-of-linkedlist.py
+++ dsa/leetcode/876-middle-of-linkedlist.py
@@ -11,7 +11,7 @@
 # Output: [4,5,6]
 # Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
 
-View on Github
+
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
