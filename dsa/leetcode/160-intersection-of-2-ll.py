f64d8d9938d1159d35556de7f7d903fdcc2db4ac
vivitruong <vivitruong1506@gmail.com>
2024-07-22 22:03:30 -0500

    add java solution for 160

--- dsa/leetcode/160-intersection-of-2-ll.py
+++ dsa/leetcode/160-intersection-of-2-ll.py
@@ -36,3 +36,16 @@ class Solution:
 
 #     return a;
 # };
+
+
+#java
+# public class Solution {
+#     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+#         ListNode a = headA, b = headB;
+#         while (a != b) {
+#             a = (a != null) ? a.next : headB;
+#             b = (b != null) ? b.next : headA;
+#         }
+#         return a;
+#     }
+# }
