4ee8432374c3af01b4315054a2065def95b26a64
vivitruong <vivitruong1506@gmail.com>
2024-08-01 22:32:55 -0500

    solution 112 in java and javascript

--- dsa/leetcode/112-path-sum.py
+++ dsa/leetcode/112-path-sum.py
@@ -11,3 +11,42 @@ class Solution:
                 return True
             return False
         return inner(root, targetSum)
+
+
+#in Java
+# class Solution {
+#     //This would be easily solved by DFS and then comparing the values
+#     public boolean dfs(TreeNode root, int targetSum, int currSum){
+#         if(root == null) return false;
+
+#         currSum += root.val;
+#         if(root.left == null && root.right == null){
+#             return (currSum == targetSum);
+#         }
+#         return dfs(root.left, targetSum, currSum) || dfs(root.right, targetSum, currSum);
+#     }
+#     public boolean hasPathSum(TreeNode root, int targetSum) {
+#         return dfs(root, targetSum, 0);
+#     }
+# }
+
+
+#in Javascript
+# var hasPathSum = function(root, targetSum) {
+
+#     const ans = [];
+#     function goDFS(node, curruntSum) {
+
+#     if(!node) return;
+
+#         if(!node.left && !node.right) {
+#             ans.push(node.val + curruntSum);
+#         }
+
+#         goDFS(node.left, curruntSum + node.val);
+#         goDFS(node.right, curruntSum + node.val);
+#     }
+#     goDFS(root, 0);
+
+#     return ans.includes(targetSum);
+# };
