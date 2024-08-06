f0a9b81227ee547eb4cc0dfb41f71b27ea244ac6
vivitruong <vivitruong1506@gmail.com>
2024-08-05 21:58:01 -0500

    add python solution

--- dsa/leetcode/1456-max-num-of-vowels-in-a-substring-of-a-givenlength.py
+++ dsa/leetcode/1456-max-num-of-vowels-in-a-substring-of-a-givenlength.py
@@ -19,3 +19,17 @@
 # Input: s = "leetcode", k = 3
 # Output: 2
 # Explanation: "lee", "eet" and "ode" contain 2 vowels.
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+        l, res, total = 0, 0, 0
+        vowels = "aeiou"
+
+        for r in range(len(s)):
+            if s[r] in vowels:
+                total += 1
+            if (r - l + 1) > k:
+                if s[l] in vowels:
+                    total -= 1
+                l += 1
+            res = max(res, total)
+        return res
