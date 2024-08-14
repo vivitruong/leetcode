399d848def45b58110651d0a38ea11f086b59f0a
vivitruong <vivitruong1506@gmail.com>
2024-08-13 22:10:58 -0500

    953

---
+++ dsa/leetcode/953-verifying-an-alien-dictionary.py
@@ -0,0 +1,35 @@
+# In an alien language, surprisingly, they also use English lowercase letters, but possibly in a different order. The order of the alphabet is some permutation of lowercase letters.
+
+# Given a sequence of words written in the alien language, and the order of the alphabet, return true if and only if the given words are sorted lexicographically in this alien language.
+
+
+
+# Example 1:
+
+# Input: words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
+# Output: true
+# Explanation: As 'h' comes before 'l' in this language, then the sequence is sorted.
+# Example 2:
+
+# Input: words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"
+# Output: false
+# Explanation: As 'd' comes after 'l' in this language, then words[0] > words[1], hence the sequence is unsorted.
+
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        # first differing char
+        # if word A is prefix of word B, word B must be AFTER word A
+        orderInd = { c : i for i, c in enumerate(order)}
+
+        for i in range(len(words) - 1):
+            w1, w2 = words[i], words[i + 1]
+
+            for j in range(len(w1)):
+                if j == len(w2):
+                    return False
+
+                if w1[j] != w2[j]:
+                    if orderInd[w2[j]] < orderInd[w1[j]]:
+                        return False
+                    break
+        return True
