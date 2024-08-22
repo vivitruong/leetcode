a9dbb00fcce14fbc11045bfbe9eb48402602496a
vivitruong <vivitruong1506@gmail.com>
2024-08-21 21:22:49 -0500

    add 71

--- dsa/leetcode/71-simplify-path.py
+++ dsa/leetcode/71-simplify-path.py
@@ -81,3 +81,36 @@ class Solution:
 
         res = "/" + "/".join(stack)
         return res
+
+
+
+
+
+#     var simplifyPath = (path, slash = '/', stack = []) => {
+#     const paths = path.split(slash).filter(Boolean);
+
+#     for (const _path of paths) traversePath(_path, stack);
+
+#     return `${slash}${stack.join(slash)}`;
+# };
+
+# const traversePath = (path, stack) => {
+#     if (canPush(path)) return stack.push(path);
+
+#     if (canPop(path, stack)) stack.pop();
+# };
+
+# const canPush = (path) => !(
+#     isCurrentDirectory(path) ||
+#     isParentDirectory(path)
+# );
+
+# const canPop = (path, stack) =>
+#     isParentDirectory(path) &&
+#     !isEmpty(stack);
+
+# const isCurrentDirectory = (path) => (path === '.');
+
+# const isParentDirectory = (path) => (path === '..');
+
+# const isEmpty = ({ length }) => (0 === length);
