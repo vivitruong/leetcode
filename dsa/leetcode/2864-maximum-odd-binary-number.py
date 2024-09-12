4ada57513592eb1bf47023e316605a7479436bcc
vivitruong <vivitruong1506@gmail.com>
2024-09-11 21:35:09 -0500

    fix

--- dsa/leetcode/2864-maximum-odd-binary-number.py
+++ dsa/leetcode/2864-maximum-odd-binary-number.py
@@ -8,16 +8,6 @@
 
 
 
-# Example 1:
-
-# Input: s = "010"
-# Output: "001"
-# Explanation: Because there is just one '1', it must be in the last position. So the answer is "001".
-# Example 2:
-
-# Input: s = "0101"
-# Output: "1001"
-# Explanation: One of the '1's must be in the last position. The maximum number that can be made with the remaining digits is "100". So the answer is "1001".
 def maximumOddBinaryNumber(self, str):
     s = [c for c in s]
     left = 0
