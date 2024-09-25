0adb24d23d24d30ef44d5fe142d87c5885e8b19b
vivitruong <vivitruong1506@gmail.com>
2024-09-24 22:14:02 -0500

    add typesript , similar to javascript

--- dsa/leetcode/344-reverse-string.py
+++ dsa/leetcode/344-reverse-string.py
@@ -41,6 +41,20 @@ class Solution:
 #     }
 # };
 
+#typescript
+# function reverseString(s: string[]): void {
+#     let l = 0;
+#     let r = s.length - 1;
+
+#     while (l < r) {
+#         let temp = s[l];
+#         s[l] = s[r];
+#         s[r] = temp;
+#         l += 1;
+#         r -= 1;
+#     }
+# }
+
 
 
 #java
