4cd2cc9702f143cf6486db98d75824aad2cced19
vivitruong <vivitruong1506@gmail.com>
2024-09-30 22:30:08 -0500

    add 909 java solution

--- dsa/leetcode/909-snakes-and-ladders.py
+++ dsa/leetcode/909-snakes-and-ladders.py
@@ -40,3 +40,39 @@ class Solution:
                     visit.add(nextSquare)
                     q.append([nextSquare, moves + 1])
         return -1
+
+
+
+        #java
+
+    #     class Solution {
+    # public int snakesAndLadders(int[][] board) {
+    #     int n = board.length;
+
+    #     reverseBoard(board);
+
+    #     boolean[] visited = new boolean[n*n+1];
+    #     Queue<int[]> q = new LinkedList<>();
+    #     q.offer(new int[]{1, 0});
+    #     visited[1] = true;
+
+    #     while(!q.isEmpty()) {
+    #         int[] curr = q.poll();
+    #         for(int j=1; j<=6; j++) {
+    #             int next = curr[0] + j;
+    #             int[] coor = squareToCoor(next, n);
+    #             if(board[coor[0]][coor[1]] != -1) {
+    #                 next = board[coor[0]][coor[1]];
+    #             }
+    #             if(next == n*n) {
+    #                 return curr[1] + 1;
+    #             }
+    #             if(!visited[next]) {
+    #                 visited[next] = true;
+    #                 q.offer(new int[]{next, curr[1] + 1});
+    #             }
+    #         }
+    #     }
+
+    #     return -1;
+    # }
