34e7d5bd4fafe88d1e2084f32264cbc82899e6d6
vivitruong <vivitruong1506@gmail.com>
2024-10-01 22:16:18 -0500

    Add

--- dsa/leetcode/918-maxximum-sum-circular-subarray.py
+++ dsa/leetcode/918-maxximum-sum-circular-subarray.py
@@ -71,3 +71,26 @@ class Solution:
 #         return globMax > 0 ? Math.max(globMax, total - globMin) : globMax;
 #     }
 # }
+
+
+
+#typescript
+# View on Github
+# function maxSubarraySumCircular(nums: number[]): number {
+#     let [globalMax, globalMin] = [nums[0], nums[0]];
+#     let [currentMax, currentMin] = [0, 0];
+#     let total = 0;
+
+#     for (let num of nums) {
+#         currentMax = Math.max(num, currentMax + num);
+#         currentMin = Math.min(num, currentMin + num);
+#         total += num;
+#         globalMax = Math.max(globalMax, currentMax);
+#         globalMin = Math.min(globalMin, currentMin);
+#     }
+
+#     return globalMax > 0 ? Math.max(globalMax, total - globalMin) : globalMax;
+# }
+
+
+#i need to check agian because aparently i still dont understand
