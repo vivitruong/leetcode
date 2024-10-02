27ca8edc49f366571f8dee760bfb1393992bf960
vivitruong <vivitruong1506@gmail.com>
2024-10-01 22:13:46 -0500

    Ad jvavascruot

--- dsa/leetcode/918-maxximum-sum-circular-subarray.py
+++ dsa/leetcode/918-maxximum-sum-circular-subarray.py
@@ -37,3 +37,20 @@ class Solution:
             globMin = min(curMin, globMin)
 
         return max(globMax, total - globMin) if globMax > 0 else globMax
+
+
+# var maxSubarraySumCircular = function (nums) {
+#     let [globalMax, globalMin] = [nums[0], nums[0]];
+#     let [currentMax, currentMin] = [0, 0];
+#     let total = 0;
+
+#     for (num of nums) {
+#         currentMax = Math.max(num, currentMax + num);
+#         currentMin = Math.min(num, currentMin + num);
+#         total += num;
+#         globalMax = Math.max(globalMax, currentMax);
+#         globalMin = Math.min(globalMin, currentMin);
+#     }
+
+#     return globalMax > 0 ? Math.max(globalMax, total - globalMin) : globalMax;
+# };
