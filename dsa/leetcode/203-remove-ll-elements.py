f8e271171c89b53684e95e7db089928489b295a2
vivitruong <vivitruong1506@gmail.com>
2024-10-02 20:26:30 -0500

    add 203

--- dsa/leetcode/203-remove-ll-elements.py
+++ dsa/leetcode/203-remove-ll-elements.py
@@ -42,3 +42,26 @@ class Solution(object):
 
             curr = nxt
         return dummy.next
+
+# add dtypescreto[t]
+# function removeElements(head: ListNode | null, val: number): ListNode | null {
+
+#     let sentinel_node : ListNode = new ListNode(0, head);
+#     let slow_pointer  : ListNode | null = sentinel_node;
+#     let fast_pointer  : ListNode | null = null;
+
+#     while (slow_pointer) {
+
+#         // get next legible node
+#         fast_pointer = slow_pointer.next;
+#         while (fast_pointer && fast_pointer.val === val) {
+#             fast_pointer = fast_pointer.next;
+#         }
+
+#         // Set next node to the legible node
+#         slow_pointer.next = fast_pointer;
+#         slow_pointer      = slow_pointer.next;
+#     }
+
+#     return sentinel_node.next;
+# };
