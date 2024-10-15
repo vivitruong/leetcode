3a1083ff5a07f1b6b0bdacc0c74e5b628ebdf175
vivitruong <vivitruong1506@gmail.com>
2024-10-14 21:32:20 -0500

    752

--- dsa/leetcode/752-open-the-lock.py
+++ dsa/leetcode/752-open-the-lock.py
@@ -26,3 +26,31 @@
 # Input: deadends = ["8887","8889","8878","8898","8788","8988","7888","9888"], target = "8888"
 # Output: -1
 # Explanation: We cannot reach the target without getting stuck.
+
+
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        if "0000" in deadends:
+            return -1
+
+        def children(wheel):
+            res = []
+            for i in range(4):
+                digit = str((int(wheel[i]) + 1) % 10)
+                res.append(wheel[:i] + digit + wheel[i + 1 :])
+                digit = str((int(wheel[i]) + 10 - 1) % 10)
+                res.append(wheel[:i] + digit + wheel[i + 1 :])
+            return res
+
+        q = deque()
+        visit = set(deadends)
+        q.append(["0000", 0])  # [wheel, turns]
+        while q:
+            wheel, turns = q.popleft()
+            if wheel == target:
+                return turns
+            for child in children(wheel):
+                if child not in visit:
+                    visit.add(child)
+                    q.append([child, turns + 1])
+        return -1
