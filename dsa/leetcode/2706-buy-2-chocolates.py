4eb04e89e1e84dd00dc48436f6047f15991f0a9c
vivitruong <vivitruong1506@gmail.com>
2024-10-23 21:42:12 -0500

    Add solution

--- dsa/leetcode/2706-buy-2-chocolates.py
+++ dsa/leetcode/2706-buy-2-chocolates.py
@@ -16,3 +16,14 @@
 # Input: prices = [3,2,3], money = 3
 # Output: 3
 # Explanation: You cannot buy 2 chocolates without going in debt, so we return 3.
+
+def buyChoco(self, prices, money):
+    min1 = min2 = float("inf")
+
+    for p in prices:
+        if p < min1:
+            min1, min2 = p, min1
+        elif p < min2:
+            min2 = p
+    leftover = money - min1 - min2
+    return leftover if leftover >= 0 else money
