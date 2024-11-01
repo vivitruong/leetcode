b14b233c95c4d21e6e8387333c2f758c210c65a5
vivitruong <vivitruong1506@gmail.com>
2024-10-31 21:56:33 -0500

    add jump game

---
+++ dsa/leetcode/jumpgame.py
@@ -0,0 +1,16 @@
+# You are given an integer array nums where each element nums[i] indicates your maximum jump length at that position.
+
+# Return true if you can reach the last index starting from index 0, or false otherwise.
+
+# Example 1:
+
+# Input: nums = [1,2,0,1,0]
+
+# Output: true
+# Explanation: First jump from index 0 to 1, then from index 1 to 3, and lastly from index 3 to 4.
+
+# Example 2:
+
+# Input: nums = [1,2,1,0,1]
+
+# Output: false
